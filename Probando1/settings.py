c8b0931bea3f006a4bfd705717ad26608241b1ed
David Mardones <david.mardones04@inacapmail.cl>
2023-06-08 00:45:25 -0400

    catalogo

--- Probando1/settings.py
+++ Probando1/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME' : 'AserraderoDB',
         'USER' : 'postgres',
-        'PASSWORD':'123456789',
+        'PASSWORD':'Dakskitlol1',
         'HOST':'localhost',
         'PORT' : '5432',
     }
