d5507f08852e2de8ca2faa518542a7a959518d51
byronvasa <byronmkp.12@gmail.com>
2023-06-07 20:11:58 -0400

    creacion js

---
+++ Probando1/static/js/tablas.js
@@ -0,0 +1,14 @@
+const listUser=async()=>{
+    try{
+        const response=await fetch("https://jsonplaceholder.typicode.com/users");
+        const data=response.json();
+        console.log(data);
+    } catch(ex) {
+        alert(ex);
+    }
+};
+
+
+window.addEventListener("load",async()=>{
+
+});
